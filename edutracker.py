b1072acaeebe1d823b8edf8c7e0bf15f59840494
Flammex <bernikovrv@gmail.com>
2023-03-04 14:38:04 +0300

    Update edutracker.py

--- edutracker.py
+++ edutracker.py
@@ -1,7 +1,16 @@
-from flask import Flask
-from flask import render_template
-from flask import request
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+    #Show all statistic
+    return render_template("main.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    #register user
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        
